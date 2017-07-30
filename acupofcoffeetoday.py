e19f1a2e3fadc9ca7810a3e40e2abf661f0cd7a3
Sangwon Kim <kittyjls13@gmail.com>
2017-07-29 20:20:07 -0700

    Added style

--- acupofcoffeetoday.py
+++ acupofcoffeetoday.py
@@ -3,41 +3,68 @@ app = Flask(__name__)
 import requests, time
 from flask import render_template, request, jsonify, json
 
+YELP_SEARCH_URL = "https://api.yelp.com/v3/businesses/search"
+YELP_ACCESS_TOKEN_URL = "https://api.yelp.com/oauth2/token"
+
 @app.route('/', methods=['GET'])
 def get_user_location():
   return render_template("index.html")
 
 @app.route('/search', methods=['GET']) 
 def get_coffee_places(): # get coffee places near user's location using Yelp API
+  # TODO: factor out access token grabbing - "token = get_access_token()"
   # get access token; if it's expired, re-request Yelp
   file = open("access_token.txt", "r+")
   file_json = file.read()
   expire_date = json.loads(file_json)['expire_date']
 
   if expire_date <= time.time():
-    access_token_url = "https://api.yelp.com/oauth2/token"
-    grant_type = "client_credentials"
+    # TODO: put this in a secure location not tracked by git
     client_id = "IN6MlR4jtubt2QFdP2oFbg"
     client_secret = "O2ag7NpZkhLqlReZkr0XlFPHgqE8VfKVQvWAsLhmSbiPsJu2lgTPSAQLuxV8mPjs"
-    access_token_response = requests.post(access_token_url, data={ "grant_type": grant_type, "client_id": client_id, "client_secret": client_secret })
+
+   # TODO: handle requests.post erroring or http status != 200
+    access_token_response = requests.post(
+      YELP_ACCESS_TOKEN_URL,
+      data={
+	"grant_type": "client_credentials",
+	"client_id": client_id,
+	"client_secret": client_secret,
+      },
+    )
     
-    access_token = access_token_response.json()['access_token']
-    expires_in = access_token_response.json()['expires_in']
+    parsed_access_token_response = access_token_response.json()
+    access_token = parsed_access_token_response['access_token']
+    expires_in = parsed_access_token_response['expires_in']
     expire_date = expires_in + time.time()
     
     # save token to the file
-    new_file_json = json.dumps({ "access_token": access_token, "expire_date": expire_date })
+    new_file_json = json.dumps({
+      "access_token": access_token,
+      "expire_date": expire_date,
+    })
     file.write(new_file_json)
     file.close()
+
   else: # else bring saved access token from the file
     access_token = json.loads(file_json)['access_token']
     file.close()
 
   # get Yelp search results
-  url = "https://api.yelp.com/v3/businesses/search"
   latitude = request.args.get('latitude')
   longitude = request.args.get('longitude')
-  params = { "latitude": latitude, "longitude": longitude }
+  sort_by = "rating" # TODO: enable different options in html, and receive chosen option
+  params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "categories": "coffee",
+    "sort_by": sort_by,
+    "open_now": True,
+  }
   headers = { "Authorization": "Bearer {}".format(access_token) }
-  response = requests.get(url, params=params, headers=headers)
+
+  # TODO: handle non-200 responses and errors
+  response = requests.get(YELP_SEARCH_URL, params=params, headers=headers)
+
+  # TODO: jsonify(success=True, error_message="", yelp_api_response=response.content)
   return jsonify(response.content) 
